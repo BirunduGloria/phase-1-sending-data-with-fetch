2453c8f0d690ab46e8526cccca8984dd7bf543a7
Gloria Birundu <birundugloria@gmail.com>
2025-06-29 18:28:48 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = data.id;
+      body.appendChild(p);
+    })
+    .catch(error => {
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      body.appendChild(p);
+    });
+}
